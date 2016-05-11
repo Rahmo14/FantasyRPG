caee183b5870bc61e5cdfc756f9824c30aab1b24
64003752 <64003752@hs-52911-macair.ad.edenpr.org>
2016-05-11 10:12:36 -0500

    Creating j frame in main class

--- src/main/MainRunner.java
+++ src/main/MainRunner.java
@@ -1,10 +1,15 @@
 package main;
+import javax.swing.JFrame;
+
 
 public class MainRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame window = new JFrame();  //Creates a frame to display graphics in
+		window.setSize(750, 480);
+		window.setTitle("Window of Characters");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
+		window.setVisible(true);
 	}
 
 }
