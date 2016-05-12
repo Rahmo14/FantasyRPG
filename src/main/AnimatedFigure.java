6f68584ce8f203b907b8f815fbaf5715b123f33e
90309024 <90309024@hs-54816-macair.ad.edenpr.org>
2016-05-12 10:42:50 -0500

    hello

---
+++ src/main/AnimatedFigure.java
@@ -0,0 +1,92 @@
+package main;
+
+import java.awt.Color;
+
+public class AnimatedFigure extends Applet {
+	private int index = 0;
+
+	    private int horizontalmove = 0; // Horizontal Move counter
+
+	    int[] horiz = {45,40,35,30};
+
+	    int[] vert = {70,75,80,80};
+	    private int sleep = 100;
+	    public void start ()
+	
+	    {
+	
+	        index = 0;
+	
+	      
+	
+	        // Initialize our horizontal move at 0
+	
+	        horizontalmove = 0;
+	
+	    }
+	
+	    public void paint (Graphics gr)
+	
+	    {
+	
+	        gr.drawLine (0,80,150,80);
+	
+	      
+
+	        // Add horizontal move to each line to make it move its x coordinate
+	
+	        gr.drawOval (10 + horizontalmove,20,30,20);
+
+	        gr.drawLine (30 + horizontalmove,40,30 + horizontalmove,60);
+
+	        gr.drawLine (30 + horizontalmove,60,30 + horizontalmove,80);
+
+	        gr.setColor (Color.WHITE);
+	
+	        gr.drawLine (30 ,60,horiz[index],vert[index]);
+	
+	        ++index;
+
+	        ++horizontalmove;
+
+	 
+
+	        if(index == horiz.length)
+		            index = 0;
+	
+	        // Once we reach 150, start back at zero.
+
+	        if (horizontalmove == 150) {
+
+	            horizontalmove = 0;
+
+	        }
+
+	        gr.setColor (Color.BLACK);
+
+	      
+
+	        // Again add the horizontal move value to x coordinates
+
+	        gr.drawLine (30 + horizontalmove,60,horiz[index] + horizontalmove,vert[index]);
+
+	        try
+
+	        {
+	        	Thread.sleep (sleep);
+
+	        }
+
+	        catch (InterruptedException e)
+
+	        {
+
+	        }
+
+	        repaint();
+
+	    }
+
+	}
+
+}
--- src/main/Graphics.java
+++ src/main/Graphics.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.awt.Color;
+
 public class Graphics {
 	
 	// Where we are going to put all the graphics
+
+	
 
 }
