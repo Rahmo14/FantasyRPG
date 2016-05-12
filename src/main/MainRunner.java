1a1791ef8c87ee57b10b7606cdc679ae6a4e2d9e
Rahmo14 <rahmom145@gmail.com>
2016-05-12 10:27:02 -0500

    Random

--- src/main/MainRunner.java
+++ src/main/MainRunner.java
@@ -1,15 +1,17 @@
 package main;
-import javax.swing.JFrame;
 
+import javax.swing.JFrame;
 
 public class MainRunner {
 
 	public static void main(String[] args) {
-		JFrame window = new JFrame();  //Creates a frame to display graphics in
+		JFrame window = new JFrame(); // Creates a frame to display graphics in
 		window.setSize(750, 480);
 		window.setTitle("Window of Characters");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		;
 		window.setVisible(true);
+		
+		// hello
 	}
-
 }
