125580d32474ebdd793fa5b2ded1123c264dd849
64003752 <64003752@hs-52911-macair.ad.edenpr.org>
2016-05-13 10:41:57 -0500

    stick

--- src/main/AnimatedFigure.java
+++ src/main/AnimatedFigure.java
@@ -19,36 +19,30 @@ public class AnimatedFigure extends Applet {
     }
     public void paint (Graphics gr)
     {
+    	
+  //**************************************Hero Stick Figure***********************************************//
         gr.drawLine (0, 400,750,400); // Horizontal Line
-	 
         gr.drawOval (20 + horizontalmove, 200, 50, 50); // Head
         gr.drawLine (45 + horizontalmove, 250, 45 + horizontalmove, 340); // Body
         gr.drawLine (45 + horizontalmove, 300, 80 + horizontalmove, 265); // Arm1
         gr.drawLine (45 + horizontalmove, 300, 80 + horizontalmove, 300); // Arm2
         gr.drawLine (45 + horizontalmove, 340, 45 + horizontalmove, 400); // Straight leg
         //gr.drawLine (45 + horizontalmove, 340, 65 + horizontalmove, 400); // Diagonal leg
-        
         gr.drawOval (77 + horizontalmove, 273, 6, 6);                     // \
         gr.drawLine (80 + horizontalmove, 273, 80 + horizontalmove, 170); //  > Sword
         gr.drawLine (70 + horizontalmove, 263, 90 + horizontalmove, 263); // /  
-        
-        gr.drawOval (70 + horizontalmove, 290, 20, 20); //  > Shield
-        gr.drawOval (57 + horizontalmove, 277, 45, 45); // / 
-       
-        
+        gr.drawOval (70 + horizontalmove, 290, 20, 20); // Shield
+        gr.drawOval (57 + horizontalmove, 277, 45, 45); // Shield     
         gr.drawLine (30 ,60,horiz[index],vert[index]);
 //        ++index;
 //        ++horizontalmove;
-
         if(index == horiz.length)
             index = 0;
-		
         // Once we reach 150, start back at zero.
         if (horizontalmove == 150) {
             horizontalmove = 0;
         }
         gr.setColor (Color.BLACK);
-	 
         // Again add the horizontal move value to x coordinates
         gr.drawLine (45 + horizontalmove, 340, 65 + horiz[index] + horizontalmove, 320 + vert[index]);
         try
@@ -59,8 +53,7 @@ public class AnimatedFigure extends Applet {
         {
         }
         repaint();
-    }
-    
-    
+        
+            }
 }
 
