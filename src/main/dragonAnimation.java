216345c210683a7804c292e4d4b2d108588b4b99
90309024 <90309024@hs-54816-macair.ad.edenpr.org>
2016-05-17 11:09:09 -0500

    Abby

--- src/main/dragonAnimation.java
+++ src/main/dragonAnimation.java
@@ -7,7 +7,7 @@ public class dragonAnimation extends JPanel {
 	@Override
     public void paintComponent(Graphics gr) {    
     	super.paintComponent(gr);
-    	
+    	gr.drawLine(550, 300, 600, 300);
     	//Add lines and whatnot here
     	
     	
